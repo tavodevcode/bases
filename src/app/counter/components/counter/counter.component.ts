d081d3522b23e570ebcdcb9d9cc3ca5d04ba2f5c
tavodevcode <tavodevcode@gmail.com>
2023-12-12 00:59:16 -0600

    Add CounterComponent to the project

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-counter',
+    templateUrl: 'Counter.component.html'
+})
+
+export class CounterComponent implements OnInit {
+    public count: number = 0;
+
+    ngOnInit() { }
+
+    public handleCountIncreased(): void {
+      this.count++;
+    }
+  
+    public handleCountDecreased(): void {
+      if(this.count === 0) return;
+  
+      this.count--;
+    }
+  
+    public handleCountReset(): void {
+      this.count = 0;
+    }
+}
