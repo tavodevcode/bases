5afe29fc75465bab81ed2a1e76eaba5fd5be2b57
tavodevcode <tavodevcode@gmail.com>
2023-12-13 01:17:49 -0600

    Add DbzModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
