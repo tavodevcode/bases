3841477b11ba8ba0d01bca8631986458e68930ee
tavodevcode <tavodevcode@gmail.com>
2023-12-13 01:18:26 -0600

    Add DbzService with character management

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core'
+import { v4 as uuid } from 'uuid'
+
+import { Character } from '../interfaces/character.interface'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Goku',
+      power: 15000
+    },
+    {
+      id: uuid(),
+      name: 'Vegeta',
+      power: 7500
+    },
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 5000
+    }
+  ]
+
+  addCharacter(character: Character): void {
+    this.characters.push({ ...character, id: uuid() })
+  }
+
+  deleteCharacter(id: string): void {
+    this.characters = this.characters.filter((character) => character.id !== id)
+  }
+}
